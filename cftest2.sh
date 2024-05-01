052c135016053f8a944a5a8adf4880feee68de1a
fatkun <cyk.cn@qq.com>
2024-05-01 22:07:11 +0800

    sh

--- cftest2.sh
+++ cftest2.sh
@@ -31,7 +31,7 @@ speedtest() {
   # st 测速线程
   # cfiptest -f xxx -st 0
   # cat ali.txt|awk -F ',' '!a[$1]++{print}'
-  cfiptest -f $file -url speed.fatkun.cloudns.ch/100m -s -mins $min_speed -maxdc 1000 -maxsc $testc -st 1 -o $TMP_RESULT_PATH
+  cfiptest -f $file -s -mins $min_speed -maxdc 1000 -maxsc $testc -st 1 -o $TMP_RESULT_PATH
 
 	cat $TMP_RESULT_PATH >> $RESULT_PATH
 
@@ -40,29 +40,20 @@ speedtest() {
 		echo "no result"
 		return
 	fi
-	if [ "IPV6" = "$info" ]; then
-	  cat $TMP_RESULT_PATH|awk -F ',' 'NR>1{
+
+  cat $TMP_RESULT_PATH|awk -F ',' 'NR>1{
     mapping["HKG"] = "HK";
+    mapping["LAX"] = "US";
     mapping["NYC"] = "US";
     mapping["SJC"] = "US";
     mapping["SIN"] = "SG";
     mapping["LHR"] = "BG";
 
     for (key in mapping) {if ($4 == key) {$4 = mapping[key];break;}}
-	  print "["$1"]:"$2"#"$4" '$info'";
-	  }'|head -n "$outc" >> $TMP_PATH
-	else
-	  cat $TMP_RESULT_PATH|awk -F ',' 'NR>1{
-    mapping["HKG"] = "HK";
-    mapping["NYC"] = "US";
-    mapping["SJC"] = "US";
-    mapping["SIN"] = "SG";
-    mapping["LHR"] = "BG";
-
-    for (key in mapping) {if ($4 == key) {$4 = mapping[key];break;}}
+    if (index($1, ":") > 0) {$1="["$1"]";}
 	  print $1":"$2"#"$4" '$info'"
 	  }'|head -n "$outc" >> $TMP_PATH
-	fi
+
 	s_end_time=$(date +%s)
   s_elapsed_time=$((s_end_time - s_start_time))
   echo "耗时: $s_elapsed_time seconds"
@@ -87,8 +78,11 @@ upload() {
 
 start_time=$(date +%s)
 init
-speedtest './input2/ali.txt' "阿里" 7 5 4
-speedtest './input2/AS41378.txt' "Kirino" 5 3 4
-speedtest './input2/ipv6.txt' "IPV6" 3 2 2
+speedtest '../input/ali.txt' "阿里" 5 5 4
+speedtest '../input/AS41378.txt' "Kirino" 3 3 4
+speedtest '../input/3258.txt' "xTom" 3 3 4
+speedtest '../input/as932.txt' "XNNET" 3 3 4
+speedtest '../input/as967.txt' "VMISS" 3 3 4
+#speedtest './input2/ipv6.txt' "IPV6" 3 2 2
 final_release
 upload
